c9e8815cc12fe8a32648e875debb4b932af24e32
Juan Daniel Galarza Rodriguez <jgalarza@cafeto.local>
2019-08-28 15:37:29 -0500

    Updates

---
+++ precache-manifest.cd23f8d7116ce959d9a75f9a28d7c1dd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d35657877db76121dde4bf9ed43696d1",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "c1ee05a3402c733c5ef2",
+    "url": "/monsters-rolodex/static/css/main.35a0d0b4.chunk.css"
+  },
+  {
+    "revision": "d81979aa8ac68af22713",
+    "url": "/monsters-rolodex/static/js/2.4ea1ec15.chunk.js"
+  },
+  {
+    "revision": "c1ee05a3402c733c5ef2",
+    "url": "/monsters-rolodex/static/js/main.65ade760.chunk.js"
+  },
+  {
+    "revision": "c7f991e6cb4746d6bd30",
+    "url": "/monsters-rolodex/static/js/runtime~main.205b164d.js"
+  }
+]);
