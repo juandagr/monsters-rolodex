901c8056da7818fc12e405e4543a5fe0edbc855b
Juan Daniel Galarza Rodriguez <jgalarza@cafeto.local>
2019-08-28 15:39:11 -0500

    adding files for github pages

--- src/App.js
+++ src/App.js
@@ -1,26 +1,55 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+
+import { CardList } from './components/card-list/card-list.component';
+import { SearchBox } from './components/search-box/search-box.component';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+
+    // define state of the component
+    this.state = {
+      monsters: [],
+      searchField: ''
+    };
+
+    // define functions context when we don't use arrow functions
+    // this.handleChange = this.handleChange.bind(this);
+  }
+
+  // This function is called after the component is mounted
+  componentDidMount(){
+    // fetch call an api and get the data => return a promise (asynchronous)
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response => response.json())
+    .then(users => this.setState({ monsters : users}));
+  }
+
+  // function to handle a change in the searchBox
+  handleChange = e => {
+    this.setState({ searchField: e.target.value }); 
+  }
+
+  render() {
+    // set variables to use in render
+    const {monsters, searchField} = this.state;
+    const filteredMonsters = monsters.filter(monster =>
+      monster.name.toLowerCase().includes(searchField.toLowerCase())
+    );
+
+    return (
+      <div className="App">
+        <h1> Monsters Rolodex </h1>
+        <SearchBox 
+          placeHolder = 'search monster'
+          handleChange = {this.handleChange}
+        />
+        <CardList monsters={filteredMonsters}/>
+      </div>
+    );
+  }
 }
 
 export default App;
